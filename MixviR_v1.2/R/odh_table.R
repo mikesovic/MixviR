4a7713498017dafa5c7f70c62fcbdbd74fdf9266
Mike Sovic <sovic.1@osu.edu>
2021-09-03 15:50:41 -0400

    updated column names to match new names in ref

--- MixviR_v1.2/R/odh_table.R
+++ MixviR_v1.2/R/odh_table.R
@@ -39,17 +39,17 @@ write_ODH_table <- function(sample.dir,
   print(samp_files)
   #get everything except deletions of length > 1
   samp_muts_odh <- samp_mutations %>%
-    dplyr::select(samp_name, ALT_ID, ALT_COUNT, TOTAL_depth) %>%
+    dplyr::select(SAMP_NAME, ALT_ID, ALT_COUNT, DP) %>%
     dplyr::slice(-grep("_del.+/.+", samp_mutations$ALT_ID))
 
   if (length(grep("_del.+/.+", samp_mutations$ALT_ID)) > 0) {
     #get deletions of length >1 and split them
     deletions <- samp_mutations %>%
       dplyr::slice(grep("_del.+/.+", samp_mutations$ALT_ID)) %>%
-      dplyr::filter(gene != "non-genic") %>%
-      dplyr::select(samp_name, ALT_ID, ALT_COUNT, TOTAL_depth) %>%
+      dplyr::filter(GENE != "non-genic") %>%
+      dplyr::select(SAMP_NAME, ALT_ID, ALT_COUNT, DP) %>%
       tidyr::separate(col = ALT_ID,
-                      into = c("gene", "ID"),
+                      into = c("GENE", "ID"),
                       sep = "_") %>%
       dplyr::mutate("ID" = gsub("del", "", ID)) %>%
       tidyr::separate(col = ID,
@@ -60,8 +60,8 @@ write_ODH_table <- function(sample.dir,
     #are getting lost in the above, so adding them back in.
     nongenic_check <- samp_mutations %>%
       dplyr::slice(grep("_del.+/.+", samp_mutations$ALT_ID)) %>%
-      dplyr::filter(gene == "non-genic") %>%
-      dplyr::select(samp_name, ALT_ID, ALT_COUNT, TOTAL_depth)
+      dplyr::filter(GENE == "non-genic") %>%
+      dplyr::select(SAMP_NAME, ALT_ID, ALT_COUNT, DP)
 
     if (nrow(nongenic_check) > 0) {
       samp_muts_odh <- dplyr::bind_rows(samp_muts_odh, nongenic_check)
@@ -85,8 +85,8 @@ write_ODH_table <- function(sample.dir,
 
     deletions <- deletions %>%
       tidyr::separate_rows(del, sep = ",") %>%
-      dplyr::mutate("ALT_ID" = paste0(gene, "_del", del)) %>%
-      dplyr::select(samp_name, ALT_ID, ALT_COUNT, TOTAL_depth)
+      dplyr::mutate("ALT_ID" = paste0(GENE, "_del", del)) %>%
+      dplyr::select(SAMP_NAME, ALT_ID, ALT_COUNT, DP)
 
     samp_muts_split_indel <- dplyr::bind_rows(samp_muts_odh, deletions)
   } else {
@@ -94,23 +94,23 @@ write_ODH_table <- function(sample.dir,
   }
 
   odh_mutations <- readr::read_tsv(mut.positions) %>%
-    dplyr::rename("ALT_ID" = "MUT",
-                  "genomic_pos" = "POS")
+    dplyr::rename("ALT_ID" = "MUT")
+             #     "genomic_pos" = "POS")
 
   #this stores all mutation calls across all samples
   all_variants <- data.frame()
 
 
-  for (file in samp_files) {
+  for (curr_file in samp_files) {
 
-    curr_samp <- file
+    curr_samp <- curr_file
 
     if (!is.null(name.sep)) {
-      curr_samp <- gsub(paste0("(.+?)", name.sep, "(.*)"), "\\1", file)
+      curr_samp <- gsub(paste0("(.+?)", name.sep, "(.*)"), "\\1", curr_file)
     }
 
     samp_muts_odh_filt <- samp_muts_split_indel %>%
-      dplyr::filter(samp_name == curr_samp)
+      dplyr::filter(SAMP_NAME == curr_samp)
 
     #get the set of mutations of interest that didn't appear in the sample
     not_in_samp <- dplyr::anti_join(x = odh_mutations,
@@ -121,31 +121,31 @@ write_ODH_table <- function(sample.dir,
 
     #get depths for positions associated with each mutation of interest not observed in the current sample
     ref_w_depth <- add_depths_to_ref(ref = ref_df,
-                                     samp = file,
+                                     samp = curr_file,
                                      samp.dir = sample.dir)
 
     depths <- ref_w_depth %>%
-      dplyr::select(genomic_pos, DP)
+      dplyr::select(POS, DP)
 
     not_in_samp <- dplyr::left_join(x = not_in_samp,
                                     y = depths,
-                                    by = "genomic_pos")
+                                    by = "POS")
 
     not_in_samp <- not_in_samp %>%
-      dplyr::mutate("samp_name" = curr_samp,
+      dplyr::mutate("SAMP_NAME" = curr_samp,
                     "ALT_COUNT" = 0,
-                    "TOTAL_depth" = DP) %>%
-      dplyr::select(samp_name, ALT_ID, ALT_COUNT, TOTAL_depth)
+                    "DP" = DP) %>%
+      dplyr::select(SAMP_NAME, ALT_ID, ALT_COUNT, DP)
 
     all_variants <- dplyr::bind_rows(all_variants, samp_muts_odh_filt, not_in_samp)
   }
 
   all_variants_summary <- all_variants %>%
-    dplyr::group_by(samp_name, ALT_ID) %>%
+    dplyr::group_by(SAMP_NAME, ALT_ID) %>%
     dplyr::summarise("alt_reads" = mean(ALT_COUNT),
-                     "total_reads" = mean(TOTAL_depth)) %>%
+                     "total_reads" = mean(DP)) %>%
     tidyr::separate(col = "ALT_ID",
-                    into = c("gene", "mutation"),
+                    into = c("GENE", "mutation"),
                     sep = "_")
 
   all_variants_summary$total_reads <- tidyr::replace_na(all_variants_summary$total_reads, 0)
@@ -157,13 +157,14 @@ write_ODH_table <- function(sample.dir,
   site_df <- readr::read_csv(site.meta, col_names = TRUE)
 
   all_variants_summary <- all_variants_summary %>%
-    tidyr::separate(col = samp_name,
+    tidyr::separate(col = SAMP_NAME,
                     into = c("codex", "date"),
                     sep = "-")
 
   all_variants_summary <- dplyr::left_join(x = all_variants_summary,
                                            y = site_df,
                                            by = "codex") %>%
+    dplyr::rename("gene" = "GENE") %>%
     dplyr::select(LAB, Site_ID, date, Facility_Name, replicate, gene, mutation, alt_reads, total_reads) %>%
     tidyr::unite(col = "sample_id",
                  Site_ID, date,
